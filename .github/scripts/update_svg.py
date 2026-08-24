28f6af222fed68fc968c522b57b495eefdf467bb
pxnsi <107796784+kirbx01@users.noreply.github.com>
2026-08-24 17:11:44 +0530

    Update update_svg.py

--- .github/scripts/update_svg.py
+++ .github/scripts/update_svg.py
@@ -122,7 +122,7 @@ def generate_svg(data):
 
     <rect x="20" y="235" width="760" height="115" class="highlight-box" />
     <text x="35" y="260" class="text-main">USER: <tspan class="green">{GITHUB_USER}</tspan>  |  OS: <tspan class="cyan">Linux / Nix / Ubuntu /Arch/ OpenBSD </tspan>  |  SHELL: <tspan class="orange">Zsh</tspan></text>
-    <text x="35" y="280" class="text-main">STACK: <tspan class="fuchsia">Go, Gin, Python, C++, Espressif IoT</tspan></text>
+    <text x="35" y="280" class="text-main">STACK: <tspan class="fuchsia">Go, Gin, Python, C++, Espressif, Arduino </tspan></text>
     <text x="35" y="300" class="text-main">$ ping {GITHUB_USER}</text>
     
     <a href="https://github.com/{GITHUB_USER}" target="_blank">

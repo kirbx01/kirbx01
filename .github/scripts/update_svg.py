d63fb7900c80b8bc16e7dab78fe864df116a07db
pxnsi <107796784+kirbx01@users.noreply.github.com>
2026-09-18 03:12:41 +0530

    fih updt

--- .github/scripts/update_svg.py
+++ .github/scripts/update_svg.py
@@ -238,7 +238,7 @@ def generate_svg(data: dict) -> str:
 
     box_inner_available_px = 760 - (35 - 20) - 15
 
-    user_line_plain = f"USER: {GITHUB_USER}  |  OS: Linux / Arch / BSD  |  SHELL: DOOM Emacs"
+    user_line_plain = f"USER: {GITHUB_USER}  |  OS: Linux / Arch / BSD  |  SHELL: fih "
     stack_line_plain = "STACK: Go, Gin, Python, C++, Espressif, Arduino"
     status_line_plain = f"Status: ONLINE  |  GitHub: github.com/{GITHUB_USER}"
 
